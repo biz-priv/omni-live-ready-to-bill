528216ef8664fda6bf6c1d279a8c26bafe6e3bb9
raviteja <raviteja.kalluri@bizcloudexperts.com>
2024-02-05 17:35:15 +0530

    delete s3 files

--- src/delete_s3_data.py
+++ src/delete_s3_data.py
@@ -16,15 +16,30 @@ def lambda_handler(event, context):
         for folder_name in folders_to_process:
             # Construct the full object key for the folder
             prefix = f"lvlp/prod/dbo/{folder_name}/"
+            print("prefix",prefix)
 
-            # List objects in the source bucket with the specified prefix
-            response = s3.list_objects_v2(Bucket=source_bucket, Prefix=prefix)
+            # Initialize the continuation token
+            continuation_token = None
 
-            # Delete objects within the folder
-            for obj in response.get('Contents', []):
-                obj_key = obj['Key']
-                # Delete the object from the source bucket
-                s3.delete_object(Bucket=source_bucket, Key=obj_key)
+            # Loop to handle pagination
+            while True:
+                # List objects in the source bucket with the specified prefix
+                if continuation_token:
+                    response = s3.list_objects_v2(Bucket=source_bucket, Prefix=prefix, ContinuationToken=continuation_token)
+                else:
+                    response = s3.list_objects_v2(Bucket=source_bucket, Prefix=prefix)
+
+                # Delete objects within the folder
+                for obj in response.get('Contents', []):
+                    obj_key = obj['Key']
+                    # Delete the object from the source bucket
+                    s3.delete_object(Bucket=source_bucket, Key=obj_key)
+
+                # Check if there are more results to fetch
+                if response.get('NextContinuationToken'):
+                    continuation_token = response['NextContinuationToken']
+                else:
+                    break  # Exit the loop if there are no more results
 
         return {
             'statusCode': 200,
